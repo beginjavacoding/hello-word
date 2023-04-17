8920dff86af38885d62f500475c1d1590f48b868
Mohammed Shafique <mohammed.shafique@lns.maersk.com>
2023-04-17 17:00:11 +0200

    updated code

--- src/main/java/com/shafique/helloworld/Main.java
+++ src/main/java/com/shafique/helloworld/Main.java
@@ -3,6 +3,6 @@ package com.shafique.helloworld;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from java");
+        System.out.println("Hello from java 17 version");
     }
 }
